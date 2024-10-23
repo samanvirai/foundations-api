76ab882926aaefdb9be77084a621a5ce5ac08cc5
Sam Rai <samanvirai@gmail.com>
2024-10-22 22:08:10 -0400

    initial files

---
+++ src/index.ts
@@ -0,0 +1,27 @@
+import { ApolloServer } from 'apollo-server';
+import { gql } from 'apollo-server';
+
+// Define your schema (GraphQL type definitions)
+const typeDefs = gql`
+  type Query {
+    hello: String
+  }
+`;
+
+// Define your resolvers
+const resolvers = {
+  Query: {
+    hello: () => 'Hello, world!',
+  },
+};
+
+// Create an instance of Apollo Server
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
+
+// Start the server
+server.listen().then(({ url }) => {
+  console.log(`🚀 Server ready at ${url}`);
+});
